7cb8149ea9e323c8461dbfe56e00930181aea6a8
Eugene Belov <eugen.alter@gmail.com>
2013-07-29 16:25:31 +0400

    Adding code agreements

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 dimension
 =========
 
-Open-source math library that provides basic geometry and algebra features
+Open-source math library that provides basic geometry and algebra features.
+
+There are some agreements in code. If you're going to modify code, please, 
+use them:
+* Code type-hinting: it's well-known that PHP has dynamic typing, but in most
+  cases you can assume type of a variable is the certain moment. Due to this
+  fact I'm using following type-hinting:
+  * $iVar   : for integer variables
+  * $fVar   : for float variables
+  * $sVar   : for string variables
+  * $rVar   : for object or resource variables
+  * $rgVar  : for array variables
+  * $fnVar  : for callable variables (since in PHP arrays could also be callable,
+              it is normal to use rg* prefix for such callbacks)
+  * $mVar   : for variables of mixed type. Note: it is a case, when type is 
+              unknown in certain moments (for example, a function parameter that
+              will be converted to some other type, if it is necessary)
+* Access level hinting: in PHP there are 3 levels of visibility: public, 
+  protected and private. So, to show in code, which access type method/variable
+  has, I'm using:
+  * __ prefix for private items, like: private function __get_something()
+  * _ prefix for protected items, like: protected function _get_something()
+  * no prefix for public items, like: public function getSomething()
+  Also public items has names with no "_" delimiter with capital letters instead.
+  Of cause, code type-hinting and access level hinting should be applied both, 
+  so full correct name of protected double property "value" will be $_fValue
--- geometry/Angle/2D.php
+++ geometry/Angle/2D.php
@@ -1,7 +1,24 @@
 <?php
+/**
+ * Represents 2d-angle entity
+ */
 class Angle_2D extends Entity_2D
 {
+    /**
+     *
+     * @_fAngle Angle value in radians 
+     */
     protected $_fAngle = null;
+    /**
+     * Construct 2d angle by passed data. There are cases:
+     * 3 Points (A,B,C): angle will be resolved as angle between AB and BC
+     * 2 Lines (AB, CD): Angle will be resolved as angle of crossing AB and CD (there will be 2 angles, but result will contain angle thet is less or equal to pi/2)
+     * 1 float: Angle will be directly set to float value
+     * After creating, angle also will be 2*pi-aligned
+     * @return null
+     * @throws Exception If resolving failed
+     * @throws LogicException If there's no corresponding resolver for given data
+     */
     public function __construct()
     {
         $rgArgs = func_get_args();
@@ -41,32 +58,51 @@ class Angle_2D extends Entity_2D
             throw new LogicException('Could not create '.__CLASS__.' instance by passed data');
         }
     }
-    
+    /**
+     * Compactibility function
+     * @param mixed $mEntity
+     */
     public function getIntersection($mEntity) 
     {
        //puppet
     }
-    
+    /**
+     * Compactibility function
+     * @param mixed $mEntity
+     */
     public function rotateCoordinates(Angle_2D $rAngle) 
     {
         //puppet
     }
-    
+    /**
+     * Compactibility function
+     * @param mixed $mEntity
+     */
     public function shiftCoordinates(Point_2D $rPoint) 
     {
         //puppet
     }
-    
+    /**
+     * Get float representation of angle in radians
+     * @return double
+     */
     public function getAngle()
     {
         return $this->_fAngle;
     }
-    
+    /**
+     * Get float representation of angle in degrees
+     * @return double
+     */
     public function getAngleDegrees()
     {
         return $this->_fAngle*180/pi();
     }
-    
+    /**
+     * Align angle using passed step (2*pi by default). Angle will be less than passed step after alignment
+     * @param double $fStep
+     * @throws LogicException If given step is non-positive
+     */
     public function normalizeAngle($fStep=null)
     {
         if(!isset($fStep))
